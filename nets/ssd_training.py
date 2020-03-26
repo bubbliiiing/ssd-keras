19ca9a1448dadac0a74d5c799ce76ce24a0dc96f
JiaQi Xu <47347516+bubbliiiing@users.noreply.github.com>
2020-03-26 17:20:38 +0800

    Update ssd_training.py

--- nets/ssd_training.py
+++ nets/ssd_training.py
@@ -23,7 +23,7 @@ class MultiboxLoss(object):
         abs_loss = tf.abs(y_true - y_pred)
         sq_loss = 0.5 * (y_true - y_pred)**2
         l1_loss = tf.where(tf.less(abs_loss, 1.0), sq_loss, abs_loss - 0.5)
-        return tf.reduce_mean(l1_loss, -1)
+        return tf.reduce_sum(l1_loss, -1)
 
     def _softmax_loss(self, y_true, y_pred):
         y_pred = tf.maximum(y_pred, 1e-7)
