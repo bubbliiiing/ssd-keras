93db5a5ebd8b00dbd62cb4ab42844261850284f4
Bubbliiiing <3323290568@qq.com>
2022-05-01 13:39:37 +0800

    update flops and training steps

--- summary.py
+++ summary.py
@@ -2,13 +2,24 @@
 #   该部分代码用于看网络结构
 #--------------------------------------------#
 from nets.ssd import SSD300
+from utils.utils import net_flops
 
 if __name__ == "__main__":
-    input_shape = [300, 300, 3]
+    input_shape = [300, 300]
     num_classes = 21
 
-    model = SSD300(input_shape, num_classes)
+    model = SSD300([input_shape[0], input_shape[1], 3], num_classes)
+    #--------------------------------------------#
+    #   查看网络结构网络结构
+    #--------------------------------------------#
     model.summary()
-
-    for i,layer in enumerate(model.layers):
-        print(i,layer.name)
+    #--------------------------------------------#
+    #   计算网络的FLOPS
+    #--------------------------------------------#
+    net_flops(model, table=False)
+    
+    #--------------------------------------------#
+    #   获得网络每个层的名称与序号
+    #--------------------------------------------#
+    # for i,layer in enumerate(model.layers):
+    #     print(i,layer.name)
