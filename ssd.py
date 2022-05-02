1ad530ea0fa2694be5eef982162c5032e9a83675
Bubbliiiing <3323290568@qq.com>
2022-05-02 20:25:54 +0800

    update comment

--- ssd.py
+++ ssd.py
@@ -61,7 +61,6 @@ class SSD(object):
     #---------------------------------------------------#
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults)
-        show_config(**self._defaults)
         for name, value in kwargs.items():
             setattr(self, name, value)
         #---------------------------------------------------#
@@ -80,6 +79,8 @@ class SSD(object):
 
         self.bbox_util = BBoxUtility(self.num_classes, nms_thresh=self.nms_iou)
         self.generate()
+        
+        show_config(**self._defaults)
 
     #---------------------------------------------------#
     #   载入模型
